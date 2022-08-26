fe1a8c68ed69defd8ca1c49bf7da2f317f10d300
jilv220 <LyuJi220@hotmail.com>
2022-08-26 19:56:31 -0400

    update domain

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,5 @@
-export const API_PROD = 'https://lbrygen-backend.xyz/api'
-export const API_BASE = 'https://lbrygen-backend.xyz/'
-export const PROD_IP = 'http://161.97.103.194/'
+export const API_PROD = 'https://lbrygen.xyz/api'
+export const API_BASE = 'https://lbrygen.xyz/'
 export const STREAM_IP = 'http://127.0.0.1:5280/'
 
 export const THUMBNAIL_OPTIMIZE = 'https://thumbnails.odycdn.com/optimize/s:390:220/quality:85/plain/'
